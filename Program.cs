c8ee583687d40c62fd793f8e0b1033ca4275d0a2
pratibhamishra11 <pratibha.mishra@accolitedigital.com>
2024-04-09 13:19:51 -0700

    uploading

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter two integer numbers : ");
+            a=Convert.ToInt32(Console.ReadLine());
+            b=Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("The sum of a and b is : "+c);
+        }
+    }
+}
